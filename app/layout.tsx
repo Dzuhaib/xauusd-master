95816da3960586dc5127908055cddbecf7f72ac1
Dzuhaib <zuhaibahmed3213951@gmail.com>
2026-05-11 13:42:10 +0500

    Fix: correct Meta Pixel ID to 1256078236308641

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2135788340532317');
+            fbq('init', '1256078236308641');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -39,7 +39,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2135788340532317&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1256078236308641&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>

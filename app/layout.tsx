f08f2f79123ef90b27ffc3f2648a36e98ec6d0c7
Zuhaib Ahmed <54768399+Dzuhaib@users.noreply.github.com>
2026-05-13 00:08:49 +0500

    Update Facebook Pixel ID in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1256078236308641');
+            fbq('init', '1899028270776905');
             fbq('track', 'PageView');
           `}
         </Script>
